25478eae6b068e0dc16d64ae24536a9d9a1bdefb
Diego <deigo555x@gmail.com>
2025-07-09 14:41:00 -0300

    feat: Adicionar menssgem de escopo e comentario

--- ommite.sh
+++ ommite.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 select tipo in "feat" "fix" "docs" "style" "refactor" "test" "chore"; do
-	if [[-n $tipo]]; then
+	if [[ -n $tipo ]]; then
 		break
 	fi
 done
+
+read -p "Digite a menssage do escopo(opcional): " escopo
+read -p "Digite a menssage do commit: " msg
+
+if [[ -z "$msg" ]]; then
+	echo "A menssagem é obrigatória!"
+	exit 1
+fi
+
+if [[ -n "$escopo" ]]; then
+	commit_msg="$tipo($escopo): $msg"
+else
+	commit_msg="$tipo: $msg"
+fi
+
+echo "$commit_msg"
+
+read -p "Deseja confirmar o commit [s/N]" confirm
+
+if [[ $confirm =~ ^[Ss]$ ]]; then
+  git commit -m "$commit_msg"
+else
+  echo "Commit cancelado."
+fi
